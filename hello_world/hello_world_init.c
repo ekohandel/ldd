d01bdec8d2c084081f73c64afafcf57ac6aae752
Abe Kohandel <abe@electronshepherds.com>
2019-12-03 07:48:24 +0000

    Add module parameters

--- hello_world/hello_world_init.c
+++ hello_world/hello_world_init.c
@@ -4,10 +4,20 @@
 #include <linux/sched.h> /* Used for current */
 MODULE_LICENSE("Dual BSD/GPL");
 
+static char *whom = "world";
+static int howmany = 1;
+
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
 static int __init hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
+    int i;
+
     printk(KERN_INFO "The process is \"%s\" (pid %i)\n", current->comm, current->pid);
+
+    for (i = 0; i < howmany; i++)
+        printk(KERN_ALERT "Hello, %s\n", whom);
 
     return 0;
 }
