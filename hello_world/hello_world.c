d5e431236536df9db860db40bd983211653823ff
Abe Kohandel <abe@electronshepherds.com>
2019-12-03 06:10:50 +0000

    Add current process information

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,10 +1,13 @@
 #include <linux/init.h>
 #include <linux/module.h>
+
+#include <linux/sched.h> /* Used for current */
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
     printk(KERN_ALERT "Hello, world\n");
+    printk(KERN_INFO "The process is \"%s\" (pid %i)\n", current->comm, current->pid);
     return 0;
 }
 
